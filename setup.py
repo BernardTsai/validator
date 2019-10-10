6d3ea4caf1ac62122a3b534f733eedf335c47b72
btsai <bernard@tsai.eu>
2019-10-10 14:58:31 +0200

    Added first setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name         = 'validate',
+    version      = '0.0.0',
+    description  = 'A script to validate a yaml modelwith the help of a json schema',
+    author       = 'Bernard Tsai',
+    author_email = 'bernard@tsai.eu',
+    url          = 'https://github.com/BernardTsai/validator',
+    py_modules   = ['validate'],
+    install_requires=[
+        "Jinja2",
+        "PyYAML",
+    ],
+)
